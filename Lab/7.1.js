f230a760421edae1c93d86eab68907d6c02c2f86
Mohamed Usaidh <164219878+Chakablaster@users.noreply.github.com>
2024-10-10 01:59:15 +0800

    7.1

--- Lab/7.1.js
+++ Lab/7.1.js
@@ -1,8 +1,8 @@
 function init() {
     // Set up the dimensions and margins of the graph
-    var margin = {top: 20, right: 30, bottom: 40, left: 100},
+    var margin = {top: 20, right: 30, bottom: 50, left: 100},
         width = 800 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        height = 450 - margin.top - margin.bottom;
 
     // Append the svg object to the body of the page
     var svg = d3.select("#chart")
@@ -25,45 +25,63 @@ function init() {
         });
 
         // Set up the x and y scales
-        var x = d3.scaleTime()
-          .domain(d3.extent(data, function(d) { return d.date; }))
-          .range([0, width]);
+        var xScale = d3.scaleTime()
+            .domain([
+                d3.min(data, function(d) { return d.date; }),
+                d3.max(data, function(d) { return d.date; })
+            ])
+            .range([0, width]);
 
-        var y = d3.scaleLinear()
-          .domain([0, d3.max(data, function(d) { return d.number; })])
-          .range([height, 0]);
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(data, function(d) { return d.number; })])
+            .range([height, 0]);
 
         // Add the x-axis
         svg.append("g")
           .attr("transform", "translate(0," + height + ")")
-          .call(d3.axisBottom(x).ticks(10));
+          .call(d3.axisBottom(xScale).ticks(10));
 
         // Add the y-axis
         svg.append("g")
-          .call(d3.axisLeft(y));
+          .call(d3.axisLeft(yScale));
 
         // Define the line
         var line = d3.line()
-          .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d.number); });
+          .x(function(d) { return xScale(d.date); })
+          .y(function(d) { return yScale(d.number); });
 
         // Add the line path
         svg.append("path")
           .datum(data)
-          .attr("fill", "none")
-          .attr("stroke", "steelblue")
-          .attr("stroke-width", 2)
+          .attr("class", "line")
           .attr("d", line);
 
-        // Add points to the chart
-        svg.selectAll("circle")
-          .data(data)
-          .enter()
-          .append("circle")
-          .attr("cx", function(d) { return x(d.date); })
-          .attr("cy", function(d) { return y(d.number); })
-          .attr("r", 3)
-          .attr("fill", "red");
+        // Define the area
+        var area = d3.area()
+          .x(function(d) { return xScale(d.date); })
+          .y0(yScale(0))  // Base of the area (0 on y-axis)
+          .y1(function(d) { return yScale(d.number); });
+
+        // Add the area path
+        svg.append("path")
+          .datum(data)
+          .attr("class", "area")
+          .attr("d", area);
+
+        // Add a horizontal line for half a million unemployed
+        svg.append("line")
+          .attr("class", "halfMilMark")
+          .attr("x1", 0)
+          .attr("y1", yScale(500000))
+          .attr("x2", width)
+          .attr("y2", yScale(500000));
+
+        // Add a label for the horizontal line
+        svg.append("text")
+          .attr("class", "halfMilLabel")
+          .attr("x", 10)
+          .attr("y", yScale(500000) - 7)
+          .text("Half a million unemployed");
 
     }).catch(function(error) {
         console.error("Error loading the data:", error);
